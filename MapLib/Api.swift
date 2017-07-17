4fed50654f034eeb569d791e67576038da130af5
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-07-18 00:47:15 +0300

    Fix drawing

--- MapLib/Api.swift
+++ MapLib/Api.swift
@@ -76,7 +76,7 @@ public class API {
             "CACHE_DIR": cacheDir,
             "SETTINGS_DIR": settingsDir,
             "SSL_CERT_FILE": certFile,
-            "NUM_THREADS": "ALL_CPUS",
+            "NUM_THREADS": "4", //"ALL_CPUS",
             "DEBUG_MODE": Constants.debugMode ? "ON" : "OFF"
         ]
         
